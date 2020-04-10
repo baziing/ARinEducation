c3df37dd5905807cf44e6fb2ad70b42b69e92b23
baziing <sarumisarumi@hotmail.com>
2020-04-10 16:06:27 +0800

    拼音

--- app/src/main/java/com/jhz/arineducation/SelectPictureActivity.java
+++ app/src/main/java/com/jhz/arineducation/SelectPictureActivity.java
@@ -20,6 +20,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -55,6 +56,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class SelectPictureActivity extends AppCompatActivity {
 
@@ -76,6 +78,9 @@ public class SelectPictureActivity extends AppCompatActivity {
     private Button ocrButton;
     private TextView textView;//输出结果
     private ImageView imageView;
+    private Button speakButton;
+
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +88,34 @@ public class SelectPictureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ocr);
 
         bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.test);
+        // 参数Context,TextToSpeech.OnInitListener
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+            /*
+                使用的是小米手机进行测试，打开设置，在系统和设备列表项中找到更多设置，
+            点击进入更多设置，在点击进入语言和输入法，见语言项列表，点击文字转语音（TTS）输出，
+            首选引擎项有三项为Pico TTs，科大讯飞语音引擎3.0，度秘语音引擎3.0。其中Pico TTS不支持
+            中文语言状态。其他两项支持中文。选择科大讯飞语音引擎3.0。进行测试。
+
+                如果自己的测试机里面没有可以读取中文的引擎，
+            那么不要紧，我在该Module包中放了一个科大讯飞语音引擎3.0.apk，将该引擎进行安装后，进入到
+            系统设置中，找到文字转语音（TTS）输出，将引擎修改为科大讯飞语音引擎3.0即可。重新启动测试
+            Demo即可体验到文字转中文语言。
+             */
+                    // setLanguage设置语言
+                    int result = tts.setLanguage(Locale.CHINA);
+                    // TextToSpeech.LANG_MISSING_DATA：表示语言的数据丢失
+                    // TextToSpeech.LANG_NOT_SUPPORTED：不支持
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+//                        Toast.makeText(this, "数据丢失或不支持", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(),"数据丢失或不支持",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
 
 //        初始化
         cameraButton=(Button)findViewById(R.id.camera);
@@ -91,8 +124,11 @@ public class SelectPictureActivity extends AppCompatActivity {
         ocrButton=(Button)findViewById(R.id.ocr);
         textView=(TextView) findViewById(R.id.result);
         imageView=(ImageView)findViewById(R.id.img);
+        speakButton=(Button)findViewById(R.id.speak);
 
         List<String> permissionList = new ArrayList<>();
+
+        init();
 
 //        开启摄像机
         cameraButton.setOnClickListener(new View.OnClickListener() {
@@ -119,6 +155,14 @@ public class SelectPictureActivity extends AppCompatActivity {
             }
         });
 
+//        speak
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                init();
+            }
+        });
+
 //        返回
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -130,6 +174,16 @@ public class SelectPictureActivity extends AppCompatActivity {
         });
 
     }
+
+    private void init(){
+        // 设置音调，值越大声音越尖（女生），值越小则变成男声,1.0是常规
+        tts.setPitch(1.0f);
+        // 设置语速
+        tts.setSpeechRate(0.5f);
+        tts.setLanguage(Locale.ENGLISH);
+        tts.speak("h", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
 
     private Boolean checkPicByte(Bitmap bitmap){
         if (bitmap.getAllocationByteCount()>128*100)
